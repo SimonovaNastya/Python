3c1cbd82bfcbdde59fb23f7b7b19170b31bfdcd2
Your Name <you@example.com>
2023-03-09 21:15:46 +0300

    Initial commit

---
+++ HW_18.6.1.py
@@ -0,0 +1,1 @@
+TOKEN = '5829644417:AAH5XWoaOvI-rTda5qAiojkzHXeK8gIm-KI'
---
+++ currency_bot/config.py
@@ -0,0 +1,6 @@
+TOKEN = '5829644417:AAH5XWoaOvI-rTda5qAiojkzHXeK8gIm-KI'
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
